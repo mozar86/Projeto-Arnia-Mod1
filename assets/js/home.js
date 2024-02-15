f8f4fba5dab02969d78212ef169fd90a7f8f16ed
Mozar Lima <89784637+mozar86@users.noreply.github.com>
2024-02-15 16:50:00 -0300

    update

---
+++ assets/js/home.js
@@ -0,0 +1,54 @@
+
+const obterProdutos = async () => {
+    
+    const requisicao = await fetch('http://localhost:3000/produtosResgatar')
+
+    const produtos = await requisicao.json()
+    
+    console.log(produtos)
+
+    return produtos
+}
+
+/*------------------------------------*/
+
+const injetarProduto = (produtos) => {
+
+    const produtoDiv = document.querySelector('.conjunto-cards')
+
+    let conteudoProduto = ''
+
+    produtos.forEach(produto => {
+        conteudoProduto = conteudoProduto + 
+        `
+        <div class="card-produto">
+            <img class="imagem-produto" src="${produto.imagem}" alt="Imagem de um ${produto.nome}">
+            <div class="descricao-produto">
+                <h3 class="titulo-produto">${produto.nome}</h3>
+            </div>
+            <div class="div-botao-card">
+            <button class="botao-card" onclick="botaoResgatar(${produto.id})" data-id="${produto.id}">Resgatar</button>
+            </div>
+        </div>
+        `
+    })
+    produtoDiv.innerHTML = conteudoProduto
+}
+
+/*------------------------------------*/
+
+const carregarProdutos = async () => {
+    
+    const produtos = await obterProdutos()
+
+    injetarProduto(produtos)
+}
+
+carregarProdutos()
+
+/*------------------------------------*/
+
+const botaoResgatar = (id) => {
+    window.location.href = `resgate.html?id=${id}`;
+}
+
