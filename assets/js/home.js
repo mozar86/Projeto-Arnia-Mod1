6ee0fddf6e7b2abb4aa2e88594e043132a320050
Mozar Lima <89784637+mozar86@users.noreply.github.com>
2024-02-16 00:27:04 -0300

    update

--- assets/js/home.js
+++ assets/js/home.js
@@ -21,13 +21,13 @@ const injetarProduto = (produtos) => {
     produtos.forEach(produto => {
         conteudoProduto = conteudoProduto + 
         `
-        <div class="card-produto">
+        <div class="card-produto" data-id="${produto.id}">
             <img class="imagem-produto" src="${produto.imagem}" alt="Imagem de um ${produto.nome}">
             <div class="descricao-produto">
                 <h3 class="titulo-produto">${produto.nome}</h3>
             </div>
             <div class="div-botao-card">
-            <button class="botao-card" onclick="botaoResgatar(${produto.id})" data-id="${produto.id}">Resgatar</button>
+            <button class="botao-card" onclick="botaoResgatarHome(${produto.id})" data-id="${produto.id}">Resgatar</button>
             </div>
         </div>
         `
@@ -48,7 +48,7 @@ carregarProdutos()
 
 /*------------------------------------*/
 
-const botaoResgatar = (id) => {
-    window.location.href = `resgate.html?id=${id}`;
+const botaoResgatarHome = (id) => {
+    window.location.href = `produtos.html?id=${id}`;
 }
 
--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -1,1 +1,54 @@
 
+const obterProduto = async () => {
+    const requisicao = await fetch('http://localhost:3000/produtosResgatar')
+
+    const produtos = await requisicao.json()
+
+    return produtos
+}
+
+/*---------------------------------------------------------------------------*/
+
+const mostrarProdutos = async (produtos) => {
+    const containerProdutos = document.getElementById('container-produtos')
+
+    try {
+        const parametros = new URLSearchParams(window.location.search)
+        const id = parametros.get('id')
+
+
+        const produto = produtos.find(produto => produto.id === id)
+
+        const conteudoProduto = `
+            <section class="imagem-produtos">
+                <img class="imagem-produto" src="${produto.imagem}" alt="Imagem de um ${produto.nome}">
+            </section>
+            <section class="info-produtos">
+                <h3 class="titulo-produto">${produto.nome}</h3>
+                <img class="preco-produto" src="../assets/images/preco-produto.png" alt="Imagem do Preço do Produto">
+                <div class="descricao-produto">
+                    <p>${produto.descricao}</p>
+                </div>
+                <button class="botao-produto" onclick="botaoResgateConfirmado('${produto.id}')" data-id="${produto.id}">Resgatar</button>
+            </section>
+        `
+
+        containerProdutos.innerHTML = conteudoProduto
+    } catch (error) {
+        console.error('Erro ao exibir produtos:', error)
+        containerProdutos.innerHTML = 'Erro ao carregar produtos.'
+    }
+}
+
+/*---------------------------------------------------------------------------*/
+
+const carregarDadosProdutos = async () => {
+    
+    const dadosProdutos = await obterProduto()
+
+    mostrarProdutos(dadosProdutos)
+}
+
+carregarDadosProdutos()
+
+
